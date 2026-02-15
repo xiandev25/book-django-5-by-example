77d0ac9f53acd499cee82b61e7a8c6238d0610b8
Xian Dev <xiandev25@gmail.com>
2026-02-16 00:33:14 +0100

    fix(project-1/chapter-01): Fix a typo in the Post model class

--- projects/1-blog-app/blog/models.py
+++ projects/1-blog-app/blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
@@ -17,6 +18,11 @@ class Post(models.Model):
         max_length=2,
         choices=Status,
         default=Status.DRAFT
+    )
+    author = models.ForeignKey(
+        settings=AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
     )
 
     class Meta:
