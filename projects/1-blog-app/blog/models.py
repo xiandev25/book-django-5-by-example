654624909b87c2214c1bf69ec651b033fc83c80a
Xian Dev <xiandev25@gmail.com>
2026-02-16 00:18:02 +0100

    fix(project-1/chapter-01): Fix a typo in the Post model class

--- projects/1-blog-app/blog/models.py
+++ projects/1-blog-app/blog/models.py
@@ -3,12 +3,21 @@ from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
 
     class Meta:
         ordering = ['-publish']
