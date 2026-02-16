944ecab7301daf96c6772e5dc39319675491a0d3
Xian Dev <xiandev25@gmail.com>
2026-02-16 20:02:53 +0100

    feat(project-1/chapter-01): Customize the display of Post objects in the admin site

--- projects/1-blog-app/blog/admin.py
+++ projects/1-blog-app/blog/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
